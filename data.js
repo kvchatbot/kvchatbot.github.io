db0e3a24eaeb684362d220fed2f716e232762ec1
kvchatbot <khianpublicmail@gmail.com>
2024-07-01 21:28:58 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const latestUpdateDate = "June 30, 2024, 12:36 AM";
+const latestUpdateDate = "July 1, 2024";
 const preTrainedData = {
     // english
     "could | could|should | should|what | what|where | where|who | who|why | why|how | how|when | when|much | much|do you|have|think|remember|birthday":["Ayaw ko makipagusap sa wikang ingles, ang gusto ko ay tagalog lamang."],
